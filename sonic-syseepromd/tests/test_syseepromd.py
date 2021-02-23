a73439de4a0fabc2c6f05869419e380266c5536d
Joe LeVeque <jolevequ@microsoft.com>
2021-02-23 20:01:02 +0000

    Full unit test coverage of signal_handler()

--- sonic-syseepromd/tests/test_syseepromd.py
+++ sonic-syseepromd/tests/test_syseepromd.py
@@ -78,130 +78,175 @@ def test_clear_db():
     assert daemon_syseepromd.eeprom_tbl.getKeys.call_count == 1
     assert daemon_syseepromd.eeprom_tbl._del.call_count == 2
 
-#@mock.patch('psud.platform_chassis', mock.MagicMock())
-#@mock.patch('psud.platform_psuutil', mock.MagicMock())
+
+def test_signal_handler():
+    daemon_syseepromd = syseepromd.DaemonSyseeprom()
+    daemon_syseepromd.stop_event.set = mock.MagicMock()
+    daemon_syseepromd.log_info = mock.MagicMock()
+    daemon_syseepromd.log_warning = mock.MagicMock()
+
+    # Test SIGHUP
+    daemon_syseepromd.signal_handler(syseepromd.signal.SIGHUP, None)
+    assert daemon_syseepromd.log_info.call_count == 1
+    daemon_syseepromd.log_info.assert_called_with("Caught SIGHUP - ignoring...")
+    assert daemon_syseepromd.log_warning.call_count == 0
+    assert daemon_syseepromd.stop_event.set.call_count == 0
+
+    # Test SIGINT
+    daemon_syseepromd.log_info.reset_mock()
+    daemon_syseepromd.log_warning.reset_mock()
+    daemon_syseepromd.stop_event.set.reset_mock()
+    daemon_syseepromd.signal_handler(syseepromd.signal.SIGINT, None)
+    assert daemon_syseepromd.log_info.call_count == 1
+    daemon_syseepromd.log_info.assert_called_with("Caught SIGINT - exiting...")
+    assert daemon_syseepromd.log_warning.call_count == 0
+    assert daemon_syseepromd.stop_event.set.call_count == 1
+
+    # Test SIGTERM
+    daemon_syseepromd.log_info.reset_mock()
+    daemon_syseepromd.log_warning.reset_mock()
+    daemon_syseepromd.stop_event.set.reset_mock()
+    daemon_syseepromd.signal_handler(syseepromd.signal.SIGTERM, None)
+    assert daemon_syseepromd.log_info.call_count == 1
+    daemon_syseepromd.log_info.assert_called_with("Caught SIGTERM - exiting...")
+    assert daemon_syseepromd.log_warning.call_count == 0
+    assert daemon_syseepromd.stop_event.set.call_count == 1
+
+    # Test an unhandled signal
+    daemon_syseepromd.log_info.reset_mock()
+    daemon_syseepromd.log_warning.reset_mock()
+    daemon_syseepromd.stop_event.set.reset_mock()
+    daemon_syseepromd.signal_handler(syseepromd.signal.SIGUSR1, None)
+    assert daemon_syseepromd.log_warning.call_count == 1
+    daemon_syseepromd.log_warning.assert_called_with("Caught unhandled signal 'SIGUSR1'")
+    assert daemon_syseepromd.log_info.call_count == 0
+    assert daemon_syseepromd.stop_event.set.call_count == 0
+
+
+#@mock.patch('syseepromd.platform_chassis', mock.MagicMock())
+#@mock.patch('syseepromd.platform_psuutil', mock.MagicMock())
 #def test_wrapper_get_num_psus():
 #    # Test new platform API is available and implemented
-#    psud._wrapper_get_num_psus()
-#    assert psud.platform_chassis.get_num_psus.call_count == 1
-#    assert psud.platform_psuutil.get_num_psus.call_count == 0
+#    syseepromd._wrapper_get_num_psus()
+#    assert syseepromd.platform_chassis.get_num_psus.call_count == 1
+#    assert syseepromd.platform_psuutil.get_num_psus.call_count == 0
 #
 #    # Test new platform API is available but not implemented
-#    psud.platform_chassis.get_num_psus.side_effect = NotImplementedError
-#    psud._wrapper_get_num_psus()
-#    assert psud.platform_chassis.get_num_psus.call_count == 2
-#    assert psud.platform_psuutil.get_num_psus.call_count == 1
+#    syseepromd.platform_chassis.get_num_psus.side_effect = NotImplementedError
+#    syseepromd._wrapper_get_num_psus()
+#    assert syseepromd.platform_chassis.get_num_psus.call_count == 2
+#    assert syseepromd.platform_psuutil.get_num_psus.call_count == 1
 #
 #    # Test new platform API not available
-#    psud.platform_chassis = None
-#    psud._wrapper_get_num_psus()
-#    assert psud.platform_psuutil.get_num_psus.call_count == 2
+#    syseepromd.platform_chassis = None
+#    syseepromd._wrapper_get_num_psus()
+#    assert syseepromd.platform_psuutil.get_num_psus.call_count == 2
 #
 #
-#@mock.patch('psud.platform_chassis', mock.MagicMock())
-#@mock.patch('psud.platform_psuutil', mock.MagicMock())
+#@mock.patch('syseepromd.platform_chassis', mock.MagicMock())
+#@mock.patch('syseepromd.platform_psuutil', mock.MagicMock())
 #def test_wrapper_get_psu_presence():
 #    # Test new platform API is available
-#    psud._wrapper_get_psu_presence(1)
-#    assert psud.platform_chassis.get_psu(0).get_presence.call_count == 1
-#    assert psud.platform_psuutil.get_psu_presence.call_count == 0
+#    syseepromd._wrapper_get_psu_presence(1)
+#    assert syseepromd.platform_chassis.get_psu(0).get_presence.call_count == 1
+#    assert syseepromd.platform_psuutil.get_psu_presence.call_count == 0
 #
 #    # Test new platform API is available but not implemented
-#    psud.platform_chassis.get_psu(0).get_presence.side_effect = NotImplementedError
-#    psud._wrapper_get_psu_presence(1)
-#    assert psud.platform_chassis.get_psu(0).get_presence.call_count == 2
-#    assert psud.platform_psuutil.get_psu_presence.call_count == 1
+#    syseepromd.platform_chassis.get_psu(0).get_presence.side_effect = NotImplementedError
+#    syseepromd._wrapper_get_psu_presence(1)
+#    assert syseepromd.platform_chassis.get_psu(0).get_presence.call_count == 2
+#    assert syseepromd.platform_psuutil.get_psu_presence.call_count == 1
 #
 #    # Test new platform API not available
-#    psud.platform_chassis = None
-#    psud._wrapper_get_psu_presence(1)
-#    assert psud.platform_psuutil.get_psu_presence.call_count == 2
-#    psud.platform_psuutil.get_psu_presence.assert_called_with(1)
+#    syseepromd.platform_chassis = None
+#    syseepromd._wrapper_get_psu_presence(1)
+#    assert syseepromd.platform_psuutil.get_psu_presence.call_count == 2
+#    syseepromd.platform_psuutil.get_psu_presence.assert_called_with(1)
 #
 #
-#@mock.patch('psud.platform_chassis', mock.MagicMock())
-#@mock.patch('psud.platform_psuutil', mock.MagicMock())
+#@mock.patch('syseepromd.platform_chassis', mock.MagicMock())
+#@mock.patch('syseepromd.platform_psuutil', mock.MagicMock())
 #def test_wrapper_get_psu_status():
 #    # Test new platform API is available
-#    psud._wrapper_get_psu_status(1)
-#    assert psud.platform_chassis.get_psu(0).get_powergood_status.call_count == 1
-#    assert psud.platform_psuutil.get_psu_status.call_count == 0
+#    syseepromd._wrapper_get_psu_status(1)
+#    assert syseepromd.platform_chassis.get_psu(0).get_powergood_status.call_count == 1
+#    assert syseepromd.platform_psuutil.get_psu_status.call_count == 0
 #
 #    # Test new platform API is available but not implemented
-#    psud.platform_chassis.get_psu(0).get_powergood_status.side_effect = NotImplementedError
-#    psud._wrapper_get_psu_status(1)
-#    assert psud.platform_chassis.get_psu(0).get_powergood_status.call_count == 2
-#    assert psud.platform_psuutil.get_psu_status.call_count == 1
+#    syseepromd.platform_chassis.get_psu(0).get_powergood_status.side_effect = NotImplementedError
+#    syseepromd._wrapper_get_psu_status(1)
+#    assert syseepromd.platform_chassis.get_psu(0).get_powergood_status.call_count == 2
+#    assert syseepromd.platform_psuutil.get_psu_status.call_count == 1
 #
 #    # Test new platform API not available
-#    psud.platform_chassis = None
-#    psud._wrapper_get_psu_status(1)
-#    assert psud.platform_psuutil.get_psu_status.call_count == 2
-#    psud.platform_psuutil.get_psu_status.assert_called_with(1)
+#    syseepromd.platform_chassis = None
+#    syseepromd._wrapper_get_psu_status(1)
+#    assert syseepromd.platform_psuutil.get_psu_status.call_count == 2
+#    syseepromd.platform_psuutil.get_psu_status.assert_called_with(1)
 #
 #
-#@mock.patch('psud._wrapper_get_psu_presence', mock.MagicMock())
-#@mock.patch('psud._wrapper_get_psu_status', mock.MagicMock())
+#@mock.patch('syseepromd._wrapper_get_psu_presence', mock.MagicMock())
+#@mock.patch('syseepromd._wrapper_get_psu_status', mock.MagicMock())
 #def test_psu_db_update():
 #    psu_tbl = mock.MagicMock()
 #
-#    psud._wrapper_get_psu_presence.return_value = True
-#    psud._wrapper_get_psu_status.return_value = True
-#    expected_fvp = psud.swsscommon.FieldValuePairs(
-#        [(psud.PSU_INFO_PRESENCE_FIELD, 'true'),
-#         (psud.PSU_INFO_STATUS_FIELD, 'true'),
+#    syseepromd._wrapper_get_psu_presence.return_value = True
+#    syseepromd._wrapper_get_psu_status.return_value = True
+#    expected_fvp = syseepromd.swsscommon.FieldValuePairs(
+#        [(syseepromd.PSU_INFO_PRESENCE_FIELD, 'true'),
+#         (syseepromd.PSU_INFO_STATUS_FIELD, 'true'),
 #         ])
-#    psud.psu_db_update(psu_tbl, 1)
+#    syseepromd.psu_db_update(psu_tbl, 1)
 #    assert psu_tbl.set.call_count == 1
-#    psu_tbl.set.assert_called_with(psud.PSU_INFO_KEY_TEMPLATE.format(1), expected_fvp)
+#    psu_tbl.set.assert_called_with(syseepromd.PSU_INFO_KEY_TEMPLATE.format(1), expected_fvp)
 #
 #    psu_tbl.set.reset_mock()
 #
-#    psud._wrapper_get_psu_presence.return_value = False
-#    psud._wrapper_get_psu_status.return_value = True
-#    expected_fvp = psud.swsscommon.FieldValuePairs(
-#        [(psud.PSU_INFO_PRESENCE_FIELD, 'false'),
-#         (psud.PSU_INFO_STATUS_FIELD, 'true'),
+#    syseepromd._wrapper_get_psu_presence.return_value = False
+#    syseepromd._wrapper_get_psu_status.return_value = True
+#    expected_fvp = syseepromd.swsscommon.FieldValuePairs(
+#        [(syseepromd.PSU_INFO_PRESENCE_FIELD, 'false'),
+#         (syseepromd.PSU_INFO_STATUS_FIELD, 'true'),
 #         ])
-#    psud.psu_db_update(psu_tbl, 1)
+#    syseepromd.psu_db_update(psu_tbl, 1)
 #    assert psu_tbl.set.call_count == 1
-#    psu_tbl.set.assert_called_with(psud.PSU_INFO_KEY_TEMPLATE.format(1), expected_fvp)
+#    psu_tbl.set.assert_called_with(syseepromd.PSU_INFO_KEY_TEMPLATE.format(1), expected_fvp)
 #
 #    psu_tbl.set.reset_mock()
 #
-#    psud._wrapper_get_psu_presence.return_value = True
-#    psud._wrapper_get_psu_status.return_value = False
-#    expected_fvp = psud.swsscommon.FieldValuePairs(
-#        [(psud.PSU_INFO_PRESENCE_FIELD, 'true'),
-#         (psud.PSU_INFO_STATUS_FIELD, 'false'),
+#    syseepromd._wrapper_get_psu_presence.return_value = True
+#    syseepromd._wrapper_get_psu_status.return_value = False
+#    expected_fvp = syseepromd.swsscommon.FieldValuePairs(
+#        [(syseepromd.PSU_INFO_PRESENCE_FIELD, 'true'),
+#         (syseepromd.PSU_INFO_STATUS_FIELD, 'false'),
 #         ])
-#    psud.psu_db_update(psu_tbl, 1)
+#    syseepromd.psu_db_update(psu_tbl, 1)
 #    assert psu_tbl.set.call_count == 1
-#    psu_tbl.set.assert_called_with(psud.PSU_INFO_KEY_TEMPLATE.format(1), expected_fvp)
+#    psu_tbl.set.assert_called_with(syseepromd.PSU_INFO_KEY_TEMPLATE.format(1), expected_fvp)
 #
 #    psu_tbl.set.reset_mock()
 #
-#    psud._wrapper_get_psu_presence.return_value = False
-#    psud._wrapper_get_psu_status.return_value = False
-#    expected_fvp = psud.swsscommon.FieldValuePairs(
-#        [(psud.PSU_INFO_PRESENCE_FIELD, 'false'),
-#         (psud.PSU_INFO_STATUS_FIELD, 'false'),
+#    syseepromd._wrapper_get_psu_presence.return_value = False
+#    syseepromd._wrapper_get_psu_status.return_value = False
+#    expected_fvp = syseepromd.swsscommon.FieldValuePairs(
+#        [(syseepromd.PSU_INFO_PRESENCE_FIELD, 'false'),
+#         (syseepromd.PSU_INFO_STATUS_FIELD, 'false'),
 #         ])
-#    psud.psu_db_update(psu_tbl, 1)
+#    syseepromd.psu_db_update(psu_tbl, 1)
 #    assert psu_tbl.set.call_count == 1
-#    psu_tbl.set.assert_called_with(psud.PSU_INFO_KEY_TEMPLATE.format(1), expected_fvp)
+#    psu_tbl.set.assert_called_with(syseepromd.PSU_INFO_KEY_TEMPLATE.format(1), expected_fvp)
 #
 #    psu_tbl.set.reset_mock()
 #
-#    psud._wrapper_get_psu_presence.return_value = True
-#    psud._wrapper_get_psu_status.return_value = True
-#    expected_fvp = psud.swsscommon.FieldValuePairs(
-#        [(psud.PSU_INFO_PRESENCE_FIELD, 'true'),
-#         (psud.PSU_INFO_STATUS_FIELD, 'true'),
+#    syseepromd._wrapper_get_psu_presence.return_value = True
+#    syseepromd._wrapper_get_psu_status.return_value = True
+#    expected_fvp = syseepromd.swsscommon.FieldValuePairs(
+#        [(syseepromd.PSU_INFO_PRESENCE_FIELD, 'true'),
+#         (syseepromd.PSU_INFO_STATUS_FIELD, 'true'),
 #         ])
-#    psud.psu_db_update(psu_tbl, 32)
+#    syseepromd.psu_db_update(psu_tbl, 32)
 #    assert psu_tbl.set.call_count == 32
-#    psu_tbl.set.assert_called_with(psud.PSU_INFO_KEY_TEMPLATE.format(32), expected_fvp)
+#    psu_tbl.set.assert_called_with(syseepromd.PSU_INFO_KEY_TEMPLATE.format(32), expected_fvp)
 #
 #
 #def test_log_on_status_changed():
@@ -210,14 +255,14 @@ def test_clear_db():
 #
 #    mock_logger = mock.MagicMock()
 #
-#    psud.log_on_status_changed(mock_logger, True, normal_log, abnormal_log)
+#    syseepromd.log_on_status_changed(mock_logger, True, normal_log, abnormal_log)
 #    assert mock_logger.log_notice.call_count == 1
 #    assert mock_logger.log_warning.call_count == 0
 #    mock_logger.log_notice.assert_called_with(normal_log)
 #
 #    mock_logger.log_notice.reset_mock()
 #
-#    psud.log_on_status_changed(mock_logger, False, normal_log, abnormal_log)
+#    syseepromd.log_on_status_changed(mock_logger, False, normal_log, abnormal_log)
 #    assert mock_logger.log_notice.call_count == 0
 #    assert mock_logger.log_warning.call_count == 1
 #    mock_logger.log_warning.assert_called_with(abnormal_log)
