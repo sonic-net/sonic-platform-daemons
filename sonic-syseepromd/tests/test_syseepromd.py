d34131ed6827969492fc4959f00a58bfd19f8846
Joe LeVeque <jolevequ@microsoft.com>
2021-02-23 18:17:55 +0000

    Full unit test coverage of post_eeprom_to_db()

--- sonic-syseepromd/tests/test_syseepromd.py
+++ sonic-syseepromd/tests/test_syseepromd.py
@@ -31,9 +31,42 @@ load_source('syseepromd', os.path.join(scripts_path, 'syseepromd'))
 import syseepromd
 
 
-def test_post_eeprom_to_db():
+def test_post_eeprom_to_db_eeprom_read_fail():
     daemon_syseepromd = syseepromd.DaemonSyseeprom()
-    daemon_syseepromd.post_eeprom_to_db()
+    daemon_syseepromd.eeprom.read_eeprom = mock.MagicMock(return_value=None)
+    daemon_syseepromd.eeprom_tbl = mock.MagicMock()
+    daemon_syseepromd.log_error = mock.MagicMock()
+
+    ret = daemon_syseepromd.post_eeprom_to_db()
+    assert ret == syseepromd.ERR_FAILED_EEPROM
+    assert daemon_syseepromd.log_error.call_count == 1
+    daemon_syseepromd.log_error.assert_called_with('Failed to read EEPROM')
+    assert daemon_syseepromd.eeprom_tbl.getKeys.call_count == 0
+
+
+def test_post_eeprom_to_db_update_fail():
+    daemon_syseepromd = syseepromd.DaemonSyseeprom()
+    daemon_syseepromd.eeprom.update_eeprom_db = mock.MagicMock(return_value=1)
+    daemon_syseepromd.eeprom_tbl = mock.MagicMock()
+    daemon_syseepromd.log_error = mock.MagicMock()
+
+    ret = daemon_syseepromd.post_eeprom_to_db()
+    assert ret == syseepromd.ERR_FAILED_UPDATE_DB
+    assert daemon_syseepromd.log_error.call_count == 1
+    daemon_syseepromd.log_error.assert_called_with('Failed to update EEPROM info in database')
+    assert daemon_syseepromd.eeprom_tbl.getKeys.call_count == 0
+
+
+def test_post_eeprom_to_db_ok():
+    daemon_syseepromd = syseepromd.DaemonSyseeprom()
+    daemon_syseepromd.eeprom.update_eeprom_db = mock.MagicMock(return_value=0)
+    daemon_syseepromd.eeprom_tbl = mock.MagicMock()
+    daemon_syseepromd.log_error = mock.MagicMock()
+
+    ret = daemon_syseepromd.post_eeprom_to_db()
+    assert ret == syseepromd.ERR_NONE
+    assert daemon_syseepromd.log_error.call_count == 0
+    assert daemon_syseepromd.eeprom_tbl.getKeys.call_count == 1
 
 #@mock.patch('psud.platform_chassis', mock.MagicMock())
 #@mock.patch('psud.platform_psuutil', mock.MagicMock())
