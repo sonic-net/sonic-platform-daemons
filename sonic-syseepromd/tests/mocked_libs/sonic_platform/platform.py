5de120909e53f783bcdab6a183702941a81abf8d
Joe LeVeque <jolevequ@microsoft.com>
2021-02-25 04:21:52 +0000

    Use autopep8

--- sonic-syseepromd/tests/mocked_libs/sonic_platform/platform.py
+++ sonic-syseepromd/tests/mocked_libs/sonic_platform/platform.py
@@ -5,6 +5,7 @@
 from sonic_platform_base.platform_base import PlatformBase
 from sonic_platform.chassis import Chassis
 
+
 class Platform(PlatformBase):
     def __init__(self):
         PlatformBase.__init__(self)
