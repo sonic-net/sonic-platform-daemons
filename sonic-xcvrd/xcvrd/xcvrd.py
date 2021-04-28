20d393f73ac7055ab0ca01c845c914ec52d8f820
Aravind Mani <aravind_m1@dell.com>
2021-04-28 07:47:01 +0530

    Address comments

--- sonic-xcvrd/xcvrd/xcvrd.py
+++ sonic-xcvrd/xcvrd/xcvrd.py
@@ -750,7 +750,7 @@ def notify_media_setting(logical_port_name, transceiver_dict,
         media_dict = get_media_settings_value(physical_port, key)
 
         if(len(media_dict) == 0):
-            helper_logger.log_error("Error in obtaining media setting for",logical_port_name)
+            helper_logger.log_error("Error in obtaining media setting for", logical_port_name)
             return
 
         fvs = swsscommon.FieldValuePairs(len(media_dict))
