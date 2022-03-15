3f018dc0c7e8c388f71df63933d414a7137fe2cf
liuh-80 <liuh@microsoft.com>
2022-03-15 08:34:27 +0000

    Fix load mock lib issue

--- sonic-pcied/tests/test_pcied.py
+++ sonic-pcied/tests/test_pcied.py
@@ -9,7 +9,6 @@ if sys.version_info.major == 3:
     from unittest import mock
 else:
     import mock
-from sonic_py_common import daemon_base, device_info
 
 from .mock_platform import MockPcieUtil
 
@@ -18,6 +17,7 @@ tests_path = os.path.dirname(os.path.abspath(__file__))
 # Add mocked_libs path so that the file under test can load mocked modules from there
 mocked_libs_path = os.path.join(tests_path, "mocked_libs")
 sys.path.insert(0, mocked_libs_path)
+from sonic_py_common import daemon_base, device_info
 
 # Add path to the file under test so that we can load it
 modules_path = os.path.dirname(tests_path)
