af1d89497454cce764918854505adf662dbc57eb
vaibhav-dahiya <vdahiya@microsoft.com>
2022-07-18 15:32:48 +0000

    fix some logs
    Signed-off-by: vaibhav-dahiya <vdahiya@microsoft.com>

--- sonic-ycabled/ycable/ycable_utilities/y_cable_helper.py
+++ sonic-ycabled/ycable/ycable_utilities/y_cable_helper.py
@@ -1424,7 +1424,7 @@ def check_identifier_presence_and_update_mux_info_entry(state_db, mux_tbl, asic_
         port_tbl[asic_id] = swsscommon.Table(config_db[asic_id], "MUX_CABLE")
 
     (status, fvs) = port_tbl[asic_index].get(logical_port_name)
-    (cable_status, cable_type) = check_mux_cable_port_type(port, port_tbl, asic_index)
+    (cable_status, cable_type) = check_mux_cable_port_type(logical_port_name, port_tbl, asic_index)
 
     if status is False:
         helper_logger.log_warning("Could not retreive fieldvalue pairs for {}, inside config_db table {}".format(logical_port_name, port_tbl[asic_index].getTableName()))
