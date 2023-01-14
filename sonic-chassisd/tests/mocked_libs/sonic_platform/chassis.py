bb45fd84a033af8d4dcb970044b2a275963d0952
judyjoseph <judy.joseph@microsoft.com>
2023-01-13 23:12:53 -0500

    Add tests for chassisd run method.

---
+++ sonic-chassisd/tests/mocked_libs/sonic_platform/chassis.py
@@ -0,0 +1,27 @@
+"""
+    Mock implementation of sonic_platform package for unit testing
+"""
+
+# TODO: Clean this up once we no longer need to support Python 2
+import sys
+if sys.version_info.major == 3:
+    from unittest import mock
+else:
+    import mock
+
+from sonic_platform_base.chassis_base import ChassisBase
+
+
+class Chassis(ChassisBase):
+    def __init__(self):
+        ChassisBase.__init__(self)
+        self.eeprom = mock.MagicMock()
+
+    def get_eeprom(self):
+        return self.eeprom
+
+    def get_my_slot(self):
+        return 1
+
+    def get_supervisor_slot(self):
+        return 1
