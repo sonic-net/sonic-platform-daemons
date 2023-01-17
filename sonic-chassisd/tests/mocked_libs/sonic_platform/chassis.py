d510e132edf28c15adbaaa3a5b6d712c341b0e14
judyjoseph <judy.joseph@microsoft.com>
2023-01-17 15:58:49 -0500

    Update for comments

--- sonic-chassisd/tests/mocked_libs/sonic_platform/chassis.py
+++ sonic-chassisd/tests/mocked_libs/sonic_platform/chassis.py
@@ -2,7 +2,6 @@
     Mock implementation of sonic_platform package for unit testing
 """
 
-# TODO: Clean this up once we no longer need to support Python 2
 import sys
 if sys.version_info.major == 3:
     from unittest import mock
