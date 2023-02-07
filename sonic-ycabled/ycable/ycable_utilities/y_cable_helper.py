2b7ee33042a731b32adbbe11a49f442096e0a36a
vaibhav-dahiya <vdahiya@microsoft.com>
2023-02-07 10:13:13 +0000

    add more tests
    Signed-off-by: vaibhav-dahiya <vdahiya@microsoft.com>

--- sonic-ycabled/ycable/ycable_utilities/y_cable_helper.py
+++ sonic-ycabled/ycable/ycable_utilities/y_cable_helper.py
@@ -2739,7 +2739,6 @@ def handle_config_mux_switchmode_arg_tbl_notification(fvp, xcvrd_config_hwmode_s
             config_mode = str(fvp_dict["config"])
 
             status = 'False'
-            result = None
             physical_port = get_ycable_physical_port_from_logical_port(port)
             if physical_port is None or physical_port == PHYSICAL_PORT_MAPPING_ERROR:
                 # error scenario update table accordingly
