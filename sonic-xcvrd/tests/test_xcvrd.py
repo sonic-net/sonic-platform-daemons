95a6247e72c365dd545a29ffb060d1e51c311d4b
Vaibhav Dahiya <vdahiya@microsoft.com>
2025-06-09 22:33:44 +0000

    add all
    Signed-off-by: Vaibhav Dahiya <vdahiya@microsoft.com>

--- sonic-xcvrd/tests/test_xcvrd.py
+++ sonic-xcvrd/tests/test_xcvrd.py
@@ -1501,6 +1501,13 @@ class TestXcvrdScript(object):
         assert found == expected_found
         assert result_dict == expected_value
 
+        # Test with unreliable_los as required
+        media_settings_parser.notify_media_setting(logical_port_name, xcvr_info_dict, xcvr_table_helper, port_mapping, True)
+        found, result = app_port_tbl.get(logical_port_name)
+        result_dict = dict(result) if result else None
+        assert found == expected_found
+        assert result_dict == expected_value
+
     @patch('xcvrd.xcvrd_utilities.optics_si_parser.g_optics_si_dict', optics_si_settings_dict)
     @patch('xcvrd.xcvrd._wrapper_get_presence', MagicMock(return_value=True))
     def test_fetch_optics_si_setting(self):
